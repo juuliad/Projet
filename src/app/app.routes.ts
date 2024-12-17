c71858265bda01e04510230aced56ba514047170
unknown <juliadbz2004@gmail.com>
2024-12-17 16:20:54 +0100

    Version 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,23 +1,21 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { AccueilComponent } from './accueil/accueil.component';
 import { AjouterComponent } from './client/ajouter/ajouter.component';
 import { ConnexionComponent } from './client/connexion/connexion.component';
 import { ConsulterComponent } from './client/consulter/consulter.component';
-import { InscriptionComponent } from './client/inscription/inscription.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/connexion', pathMatch: 'full' },
+  { path: 'accueil', component: AccueilComponent },
   { path: 'ajouter', component: AjouterComponent },
   { path: 'connexion', component: ConnexionComponent },
-  { path: 'inscription', component: InscriptionComponent },
   { path: 'consulter', component: ConsulterComponent },
-  { path: 'accueil', component: AccueilComponent },
+  {
+    path: 'inscription',
+    loadComponent: () => import('./client/inscription/inscription.component').then(m => m.InscriptionComponent)
+  },
+  {
+    path: 'ajouter',
+    loadComponent: () => import('./client/ajouter/ajouter.component').then(m => m.AjouterComponent)
+  },
 ];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true })],
-  exports: [RouterModule]
-})
-
-export class AppRoutingModule { }
--- src/app/client/ajouter/ajouter.component.ts
+++ src/app/client/ajouter/ajouter.component.ts
@@ -1,12 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-ajouter',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './ajouter.component.html',
-  styleUrl: './ajouter.component.css'
+  styleUrls: ['./ajouter.component.css']
 })
 export class AjouterComponent {
+  fraisForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.fraisForm = this.fb.group({
+      libelle: ['', Validators.required],
+      montant: [null, [Validators.required, Validators.min(0)]],
+      date: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.fraisForm.valid) {
+      const noteDeFrais = this.fraisForm.value;
+      console.log('Nouvelle note de frais ajoutée :', noteDeFrais);
+      alert(`La note de frais "${noteDeFrais.libelle}" de ${noteDeFrais.montant}€ a été ajoutée avec succès !`);
+      this.fraisForm.reset();
+    }
+  }
 }
