cfa2ac958ed6da61050a3f8ef2306263e7bf0f57
Thanarol <nathanakgol@gmail.com>
2025-02-11 14:09:27 +0100

    modif navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,8 @@ import { ConnexionComponent } from './client/connexion/connexion.component';
 import { ConsulterComponent } from './client/consulter/consulter.component';
 import { InscriptionComponent } from './client/inscription/inscription.component';
 import { AjouterComponent } from './client/ajouter/ajouter.component';
+import { ValidationComponent } from './comptable/validation/validation.component';
+
 
 
 export const routes: Routes = [
@@ -14,6 +16,7 @@ export const routes: Routes = [
   { path: 'inscription', component: InscriptionComponent },
   { path: 'consulter', component: ConsulterComponent },
   {path: 'accueil', component: AccueilComponent},
+  {path: 'comptable', component: ValidationComponent},
 ];
 
 @NgModule({
